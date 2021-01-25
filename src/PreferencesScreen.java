3732851ea8685dabd7ec155a68a65976fa399a5e
Sunny Jiao <sunnyjiao888@gmail.com>
2021-01-24 22:51:27 -0500

    removed unused imports

--- src/PreferencesScreen.java
+++ src/PreferencesScreen.java
@@ -1,8 +1,6 @@
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -15,8 +13,6 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class PreferencesScreen extends ScreenAdapter {
 
-    private SpriteBatch batch;
-    private TextureAtlas atlas;
     private Stage stage;
     private DuberCore dubercore;
     TextField usernameTextField;
